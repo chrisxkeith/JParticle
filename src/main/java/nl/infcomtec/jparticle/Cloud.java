0383be4e797fe5d1ce6e032c62da0f42d601a115
CK <chris.keith@gmail.com>
2019-05-08 13:13:46 -0700

    Try restarting event listeners when hitting an IOException.

--- src/main/java/nl/infcomtec/jparticle/Cloud.java
+++ src/main/java/nl/infcomtec/jparticle/Cloud.java
@@ -454,55 +454,51 @@ public class Cloud {
             this.mine = mine;
         }
 
-        @Override
-        public void run() {
-            Thread.currentThread().setName("PublishedReader:" + mine);
-            try {
-                URL url = new URL(mine ? "https://api.particle.io/v1/devices/events" : "https://api.particle.io/v1/events");
-                HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
-                conn.setRequestMethod("GET");
-                conn.setRequestProperty("Authorization", Cloud.this.accessToken);
-                conn.setDoOutput(false);
-                try (BufferedReader bfr = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
-                    String s;
-                    while (null != (s = bfr.readLine())) {
-                        if (s.startsWith("event: ")) {
-                            String eventName = s.substring(7);
-                            String data = bfr.readLine();
-                            //System.out.println(eventName + " " + data);
-                            if (data.startsWith("data: ")) {
-                                synchronized (callBacks) {
-                                    AnyJSON aj = new AnyJSON(data.substring(6));
-                                    final Event e = new Event(devices, eventName, aj.getObject());
-                                    //System.out.println(Thread.currentThread().getName()+" "+e);
-                                    for (final DeviceEvent cb : callBacks.values()) {
-                                        if (null != cb.forDeviceName() && cb.forDeviceName().equals(e.deviceName)) {
-                                            if (null == cb.forEventName() || e.eventName.equals(cb.forEventName())) {
-                                                pool.submit(new Runnable() {
-                                                    @Override
-                                                    public void run() {
-                                                        cb.event(e);
-                                                    }
-                                                });
-                                            }
-                                        } else if (null != cb.forDeviceId() && cb.forDeviceId().equals(e.coreId)) {
-                                            if (null == cb.forEventName() || e.eventName.equals(cb.forEventName())) {
-                                                pool.submit(new Runnable() {
-                                                    @Override
-                                                    public void run() {
-                                                        cb.event(e);
-                                                    }
-                                                });
-                                            }
-                                        } else if (null == cb.forDeviceName() && null == cb.forDeviceId()) {
-                                            if (null == cb.forEventName() || e.eventName.equals(cb.forEventName())) {
-                                                pool.submit(new Runnable() {
-                                                    @Override
-                                                    public void run() {
-                                                        cb.event(e);
-                                                    }
-                                                });
-                                            }
+        private void doRun() throws Exception {
+            URL url = new URL(mine ? "https://api.particle.io/v1/devices/events" : "https://api.particle.io/v1/events");
+            HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Authorization", Cloud.this.accessToken);
+            conn.setDoOutput(false);
+            try (BufferedReader bfr = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+                String s;
+                while (null != (s = bfr.readLine())) {
+                    if (s.startsWith("event: ")) {
+                        String eventName = s.substring(7);
+                        String data = bfr.readLine();
+                        //System.out.println(eventName + " " + data);
+                        if (data.startsWith("data: ")) {
+                            synchronized (callBacks) {
+                                AnyJSON aj = new AnyJSON(data.substring(6));
+                                final Event e = new Event(devices, eventName, aj.getObject());
+                                //System.out.println(Thread.currentThread().getName()+" "+e);
+                                for (final DeviceEvent cb : callBacks.values()) {
+                                    if (null != cb.forDeviceName() && cb.forDeviceName().equals(e.deviceName)) {
+                                        if (null == cb.forEventName() || e.eventName.equals(cb.forEventName())) {
+                                            pool.submit(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                    cb.event(e);
+                                                }
+                                            });
+                                        }
+                                    } else if (null != cb.forDeviceId() && cb.forDeviceId().equals(e.coreId)) {
+                                        if (null == cb.forEventName() || e.eventName.equals(cb.forEventName())) {
+                                            pool.submit(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                    cb.event(e);
+                                                }
+                                            });
+                                        }
+                                    } else if (null == cb.forDeviceName() && null == cb.forDeviceId()) {
+                                        if (null == cb.forEventName() || e.eventName.equals(cb.forEventName())) {
+                                            pool.submit(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                    cb.event(e);
+                                                }
+                                            });
                                         }
                                     }
                                 }
@@ -510,6 +506,14 @@ public class Cloud {
                         }
                     }
                 }
+            }
+        }
+
+        @Override
+        public void run() {
+            Thread.currentThread().setName("PublishedReader:" + mine);
+            try {
+                doRun();
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
